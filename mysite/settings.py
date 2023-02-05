39a010a0796b8de5b6ad35e65812a39f4a547a30
Kyle Phan <kyledphan@gmail.com>
2023-02-05 01:21:59 -0500

    changes to environment variables

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,10 +31,10 @@ dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
