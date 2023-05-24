c538d659d6c28ae2972127976fd60ac96fd5bc16
Kyle Phan <kyledphan@gmail.com>
2023-05-23 18:04:16 -0700

    Upload postgresql database

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
